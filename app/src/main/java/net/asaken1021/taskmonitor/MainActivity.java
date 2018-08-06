0322ba8522beed1a6935708733de510d6d4e4d30
asaken1021 <asaken1021@outlook.com>
2018-08-06 16:50:53 +0900

    First Commit

---
+++ app/src/main/java/net/asaken1021/taskmonitor/MainActivity.java
@@ -0,0 +1,60 @@
+package net.asaken1021.taskmonitor;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
+    private Button stopButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.PACKAGE_USAGE_STATS}, 0);
+
+//        checkPermission();
+    }
+
+    public void startService(View v) {
+        startService(new Intent(MainActivity.this, TaskMonitorClass.class));
+    }
+
+    public void stopService(View v) {
+        stopService(new Intent(MainActivity.this, TaskMonitorClass.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(MainActivity.this, TaskMonitorClass.class));
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.PACKAGE_USAGE_STATS) != PackageManager.PERMISSION_GRANTED) {
+            /*
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.PACKAGE_USAGE_STATS)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.PACKAGE_USAGE_STATS}, 0);
+            }*/
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.PACKAGE_USAGE_STATS}, 0);
+            Log.d("checkPermission", "called");
+        }
+    }
+}
---
+++ app/src/main/java/net/asaken1021/taskmonitor/TaskMonitorClass.java
@@ -0,0 +1,64 @@
+package net.asaken1021.taskmonitor;
+
+import android.app.ActivityManager;
+import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.content.Context.ACTIVITY_SERVICE;
+
+public class TaskMonitorClass extends Service {
+
+    private Timer timer;
+    List<String> packages;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public int onStartCommand(Intent intent, int flags, int resId) {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                long start = System.currentTimeMillis() - 60000;
+                long end = System.currentTimeMillis();
+                UsageStatsManager usageStatsManager = (UsageStatsManager) getApplicationContext().getSystemService(Context.USAGE_STATS_SERVICE);
+                List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST, start, end);
+                getUsageStats(stats);
+                Log.d("Timer", "^q^");
+            }
+        }, 0, 5000);
+        return android.app.Service.START_STICKY;
+    }
+
+    public void getUsageStats(List<UsageStats> list) {
+        for (UsageStats stat : list) {
+            packages.add(stat.getPackageName());
+            Log.d("PackageName", stat.getPackageName());
+        }
+        Log.d("getUsageStats", "called");
+    }
+
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+}
