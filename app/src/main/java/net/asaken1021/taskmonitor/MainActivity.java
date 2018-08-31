bf630336129786b3433538dd71acedf97469709f
asaken1021 <asaken1021@outlook.com>
2018-08-31 23:08:22 +0900

    Commit 4

--- app/src/main/java/net/asaken1021/taskmonitor/MainActivity.java
+++ app/src/main/java/net/asaken1021/taskmonitor/MainActivity.java
@@ -1,6 +1,7 @@
 package net.asaken1021.taskmonitor;
 
 import android.Manifest;
+import android.app.usage.UsageEvents;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
@@ -9,14 +10,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button startButton;
     private Button stopButton;
+    private Button reloadButton;
+
+    private static ListView UsedAppList;
+
+    public static List<String> UsedApps = new ArrayList<>();
+    public static List<String> UsedAppNames = new ArrayList<>();
+    public ArrayAdapter<String> arrayAdapter;
+
+    public static HashMap<String, String> reversed_appKey = new HashMap<String, String>();
+
+    boolean isIntentAllowed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +45,62 @@ public class MainActivity extends AppCompatActivity {
 
         startButton = (Button) findViewById(R.id.startButton);
         stopButton = (Button) findViewById(R.id.stopButton);
+        reloadButton = (Button) findViewById(R.id.reloadButton);
+        UsedAppList = (ListView) findViewById(R.id.UsedAppList);
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.PACKAGE_USAGE_STATS}, 1);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, UsedApps);
 
-//        checkPermission();
+        UsedAppList.setAdapter(arrayAdapter);
+
+        UsedAppList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long resId) {
+                String packagename = arrayAdapter.getItem(position);
+                Log.d("Adapter->onItemClick", packagename);
+                PackageManager packageManager = getPackageManager();
+                try {
+                    packagename = reversed_appKey.get(packagename);
+                    Intent intent = packageManager.getLaunchIntentForPackage(packagename);
+                    startActivity(intent);
+                } catch (NullPointerException e) {
+                    Toast.makeText(getApplicationContext(), "このアプリは起動できません。", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+
+        checkPermission();
+
+        arrayAdapter.add("使用履歴の読み取り中...");
+
+        startService(new Intent(getApplicationContext(), TaskMonitorClass.class));
     }
 
-    public void startService(View v) {
-        startService(new Intent(MainActivity.this, TaskMonitorClass.class));
+    public void service_start(View v) {
+        startService(new Intent(getApplicationContext(), TaskMonitorClass.class));
     }
 
-    public void stopService(View v) {
-        stopService(new Intent(MainActivity.this, TaskMonitorClass.class));
+    public void service_stop(View v) {
+        stopService(new Intent(getApplicationContext(), TaskMonitorClass.class));
+    }
+
+    public void reload(View v) {
+        boolean isFirstAdd = true;
+        for (String UsedAppName : UsedAppNames) {
+            if (UsedAppName == "") {
+                return;
+            }
+            if (isFirstAdd) {
+                arrayAdapter.clear();
+                isFirstAdd = false;
+            }
+            arrayAdapter.add(UsedAppName);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        stopService(new Intent(MainActivity.this, TaskMonitorClass.class));
+        stopService(new Intent(getApplicationContext(), TaskMonitorClass.class));
     }
 
     private void checkPermission() {
@@ -54,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.PACKAGE_USAGE_STATS}, 0);
             }*/
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.PACKAGE_USAGE_STATS}, 0);
-            Log.d("checkPermission", "called");
         }
+        Log.d("checkPermission", "called");
     }
 }
--- app/src/main/java/net/asaken1021/taskmonitor/TaskMonitorClass.java
+++ app/src/main/java/net/asaken1021/taskmonitor/TaskMonitorClass.java
@@ -2,52 +2,116 @@ package net.asaken1021.taskmonitor;
 
 import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageEvents;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import static android.content.Context.ACTIVITY_SERVICE;
 
 public class TaskMonitorClass extends Service {
 
     private Timer timer;
     List<String> packages = new ArrayList<>();
+    List<String> appNames = new ArrayList<>();
+
+    PackageManager pm;
+    List<ApplicationInfo> appList = new ArrayList<>();
+    public static HashMap<String, String> appKey = new HashMap<String, String>();
+    public static HashMap<String, String> reversed_appKey = new HashMap<String, String>();
+
+    UsageStatsManager usageStatsManager;
+    List<UsageStats> stats = new ArrayList<>();
+
+    ActivityManager activityManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        pm = getPackageManager();
+        appList = pm.getInstalledApplications(0);
+        String[] installedApps = new String[appList.size() + 1];
+        int x = 0;
+        for (ApplicationInfo info : appList) {
+            installedApps[x++] = info.packageName;
+            appKey.put(info.packageName, info.loadLabel(pm).toString());
+            reversed_appKey.put(info.loadLabel(pm).toString(), info.packageName);
+        }
+        activityManager = (ActivityManager) getSystemService(getApplicationContext().ACTIVITY_SERVICE);
     }
 
     public int onStartCommand(Intent intent, int flags, int resId) {
+        quickStart();
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 long start = System.currentTimeMillis() - 60000;
                 long end = System.currentTimeMillis();
-                UsageStatsManager usageStatsManager = (UsageStatsManager) getApplicationContext().getSystemService(Context.USAGE_STATS_SERVICE);
-                List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST, start, end);
+                usageStatsManager = (UsageStatsManager) getApplicationContext().getSystemService(Context.USAGE_STATS_SERVICE);
+                stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, start, end);
                 getUsageStats(stats);
-                Log.d("Timer", "^q^");
+                getAppNames(packages);
+                try {
+                    MainActivity.UsedApps = packages;
+                    MainActivity.UsedAppNames = appNames;
+                    MainActivity.reversed_appKey = reversed_appKey;
+                } catch (NullPointerException e){
+                    //何もしない
+                }
+                packages = new ArrayList<>();
+                appNames = new ArrayList<>();
             }
         }, 0, 5000);
         return android.app.Service.START_STICKY;
     }
 
-    public void getUsageStats(List<UsageStats> list) {
-        for (UsageStats stat : list) {
-            packages.add(stat.getPackageName());
-            Log.d("PackageName", stat.getPackageName());
-        }
+    private void getUsageStats(List<UsageStats> list) {
         Log.d("getUsageStats", "called");
+        for (UsageStats stat : list) {
+            if (stat.getLastTimeUsed() > System.currentTimeMillis() - 120000) { //直近2分以内に使用が開始された場合
+                packages.add(stat.getPackageName());
+                Log.d("PackageName", stat.getPackageName());
+            }
+        }
+    }
+
+    private void getAppNames(List<String> packageNames) {
+        Log.d("getAppNames", "called");
+        for (String packageName : packageNames) {
+            String appName = appKey.get(packageName);
+            appNames.add(appName);
+            Log.d("AppName", appName);
+        }
+    }
+
+    private void quickStart() {
+        long start = System.currentTimeMillis() - 60000;
+        long end = System.currentTimeMillis();
+        usageStatsManager = (UsageStatsManager) getApplicationContext().getSystemService(Context.USAGE_STATS_SERVICE);
+        stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, start, end);
+        getUsageStats(stats);
+        getAppNames(packages);
+//                checkUsedAppStatus();
+        try {
+            MainActivity.UsedApps = packages;
+            MainActivity.UsedAppNames = appNames;
+            MainActivity.reversed_appKey = reversed_appKey;
+        } catch (NullPointerException e){
+            //何もしない
+        }
+        packages = new ArrayList<>();
+        appNames = new ArrayList<>();
     }
 
     public IBinder onBind(Intent arg0) {
